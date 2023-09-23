e84f3ba6c6418b4673d8af461935488f5e3a3127
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-09-23 17:39:51 -0400

    app fully functional

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,8 @@ mongoose.connect(
         useUnifiedTopology: true,
     }
 );
+
+// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
 module.exports = mongoose.connection;
